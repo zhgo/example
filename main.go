5ae1de74ff5fce466a81c1620f892806ed1ce9e3
liudng <liudng@gmail.com>
2015-04-07 12:08:06 +0200

    Update repo.

--- main.go
+++ main.go
@@ -3,28 +3,33 @@ package main
 import (
 	_ "github.com/zhgo/example/web/collab"
 	_ "github.com/zhgo/example/web/passport"
+	"github.com/zhgo/kernel"
 	"github.com/zhgo/web"
+	"github.com/zhgo/console"
 	"log"
+    "flag"
 )
 
-func init() {
-	///web.App.Init(web.WorkingDir + "/suite.json")
-}
-
 func main() {
-	log.Printf("%s\n", web.WorkingDir)
+	log.Printf("%s\n", kernel.WorkingDir)
 
-	/*var c string
-	flag.StringVar(&c, "c", web.WorkingDir+"/suite.json", "Usage: mplus -c=/path/to/examples.json")
+	var c, p, a string
+	flag.StringVar(&c, "c", kernel.WorkingDir+"/example.json", "Usage: mplus -c=/path/to/example.json")
+    flag.StringVar(&p, "p", "", "Usage: example -p=Passport/User/Login")
+    flag.StringVar(&a, "a", "", "Usage: example -a=1/2/3")
 	flag.Parse()
 
-	web.App.Init(c)*/
-
-	//Start HTTP server
-	web.App.NewServer(func(r *web.Request) web.Result {
-		//it will be called before run to action body, it's used to check the purview.
-		//...
-
-		return web.Result{1, ""}
-	})
+    if p == "" {
+        web.App.Init(c)
+        web.App.Load(func(r *web.Request) web.Result {
+            //it will be called before run to action body, it's used to check the purview.
+            //...
+            return web.Result{1, ""}
+        })
+        web.App.Start()
+    } else {
+        console.App.Init(c)
+        console.App.Load(p, a)
+        console.App.Start()
+    }
 }
