be16318462f1f789033250febdda5a2530674d70
liudng <liudng@gmail.com>
2015-07-30 13:41:46 +0200

    material-ui

--- frontend/gulp/tasks/browserify.js
+++ frontend/gulp/tasks/browserify.js
@@ -1,20 +1,26 @@
 var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
+    handleErrors = require('../util/handleErrors'),
     browserify = require('browserify'),
     config = require('../config').browserify,
-    factor = require('factor-bundle');
+    factor = require('factor-bundle'),
+    babelify = require('babelify');
 
 gulp.task('browserify', function(){
 
-    return browserify({
-        entries: ['./src/app/app.jsx', './src/app/x.js', './src/app/y.js'],
+    var bundler = browserify({
+        entries: ['./src/app/app1.jsx', './src/app/app2.jsx', './src/app/x.js', './src/app/y.js'],
         debug: config.debug
-    })
-    .plugin(factor, {
+    });
+
+    bundler.transform(babelify.configure({stage: 1}));
+
+    return bundler.plugin(factor, {
         // File output order must match entry order
-        o: ['./bundle/app/app.js', './bundle/app/x.js', './bundle/app/y.js']
+        o: ['./bundle/app/app1.js', './bundle/app/app2.js', './bundle/app/x.js', './bundle/app/y.js']
     })
     .bundle()
+    .on('error', handleErrors)
     .pipe(source('app/common.js'))
     .pipe(gulp.dest('./bundle'));
 });
