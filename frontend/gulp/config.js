db9e437e3fb027186f0b0ec4164f2b3a918cef02
liudng <liudng@gmail.com>
2015-08-04 16:20:12 +0200

    Merged.

--- frontend/gulp/config.js
+++ frontend/gulp/config.js
@@ -6,15 +6,10 @@ module.exports = {
     dest: "../public/app",
     entries: [
       'collab/index_index.jsx',
-<<<<<<< HEAD
-      'collab/index_signin.jsx',
-      'shared/bootstrap.jsx'
-=======
       'collab/issue_add.jsx',
       'collab/issue_browse.jsx',
       'collab/issue_detail.jsx',
       'bootstrap.jsx'
->>>>>>> dfb45c76df9fecf8a7bc200a03fb6f75253ac382
     ]
   }
 };
